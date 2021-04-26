560189299721208504d754ecbada380d873d2f6d
unknowncode44 <matiaz.orellana@gmail.com>
2021-04-25 22:59:18 -0300

    first commit

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,93 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      theme: ThemeData(
+          primaryColor: Colors.lightGreen, accentColor: Colors.greenAccent),
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    ));
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5), () => print('Splash Done / Splash Listo!'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(color: Colors.greenAccent),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundColor: Colors.black38,
+                        radius: 50.0,
+                        child: Icon(
+                          Icons.face_unlock_outlined,
+                          color: Colors.white,
+                          size: 50.0,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Text("Matias SplashScreen",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 22.0,
+                            fontWeight: FontWeight.bold,
+                          )),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      ),
+                    CircularProgressIndicator(
+                      backgroundColor: Colors.white,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                    ),
+                    Text(
+                      "Trying the wonders of Dart",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
